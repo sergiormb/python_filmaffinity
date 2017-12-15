253fbb3e1d50e29bf7605bdcc7976b277e54095e
Pol Canelles <pol@canellestudi.com>
2017-12-15 22:07:20 +0100

    Adds more data to the scraped result for the DetailPage (duration, country, genre, awards and reviews)

--- python_filmaffinity/pages/detail.py
+++ python_filmaffinity/pages/detail.py
@@ -70,3 +70,54 @@ class DetailPage(Page):
             director = director_cell.find("span", {"itemprop": 'name'})
             directors.append(director.get_text())
         return directors
+
+    def get_duration(self):
+        """Get Duration."""
+        dc = self.soup.find("dd", {"itemprop": 'duration'})
+        if not dc:
+            return None
+        return dc.get_text()
+
+    def get_country(self):
+        """Get the country."""
+        dc = self.soup.find("span", {"id": 'country-img'})
+        if not dc:
+            return None
+        return dc.img['title']
+
+    def get_genre(self):
+        """Get the genre."""
+        dc = self.soup.find("span", {"itemprop": 'genre'})
+        if not dc:
+            return None
+        genres = [i.get_text() for i in dc.find_all("a")]
+        return ', '.join(genres) if genres else None
+
+    def get_awards(self):
+        """Get the awards.
+
+        Returns:
+            TYPE: List of dicts (dict keys: year, award)
+        """
+        awards = []
+        ac = self.soup.find("dd", {"class": 'award'})
+        if not ac:
+            return awards
+        for i in ac.find_all("a"):
+            awards.append({'year': i.get_text(),
+                           'award': i.next_sibling[2:]})
+        return awards
+
+    def get_reviews(self):
+        """Get the critics reviews.
+
+        Returns:
+            TYPE: List of dicts (dict keys: author, review, url)
+        """
+        reviews = []
+        for i in self.soup.find_all("div", {"class": 'pro-review'}):
+            reviews.append(
+                {'author': i.find("div", {"itemprop": 'author'}).get_text(),
+                 'review': i.find("div", {"itemprop": 'reviewBody'}).get_text(),
+                 'url': i.a['href'] if i.a else None})
+        return reviews
