c044d004c67a6a5c10379d70d2093d0054e5fdea
Sergio Pino Márquez <sergiormb88@gmail.com>
2018-10-07 10:39:32 +0200

    Convert to list

--- python_filmaffinity/proxies.py
+++ python_filmaffinity/proxies.py
@@ -10,10 +10,10 @@ def get_random_proxy():
     proxy = {}
     response = requests.get("https://www.sslproxies.org/")
     soup = BeautifulSoup(response.text, "html.parser")
-    https_proxies = filter(
+    https_proxies = list(filter(
         lambda item: "yes" in item.text,
         soup.select("table.table tr")
-    )
+    ))
     if https_proxies:
         http_proxy = https_proxies[0]
         proxy = {'http': 'https://{}:{}'.format(
