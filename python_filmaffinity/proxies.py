ed74e4635a49e4f7ed3e5d6c9ad1685d6154c2a6
Sergio Pino Márquez <sergiormb88@gmail.com>
2018-10-07 10:25:14 +0200

    Update proxies

--- python_filmaffinity/proxies.py
+++ python_filmaffinity/proxies.py
@@ -7,14 +7,17 @@ import random
 
 
 def get_random_proxy():
+    proxy = {}
     response = requests.get("https://www.sslproxies.org/")
     soup = BeautifulSoup(response.text, "html.parser")
     https_proxies = filter(
         lambda item: "yes" in item.text,
         soup.select("table.table tr")
     )
-    http_proxy = random.choice(https_proxies)
-    return {'http': 'https://{}:{}'.format(
-        http_proxy.select_one("td").text,
-        http_proxy.select_one("td:nth-of-type(2)").text,
-    )}
+    if https_proxies:
+        http_proxy = https_proxies[0]
+        proxy = {'http': 'https://{}:{}'.format(
+            http_proxy.select_one("td").text,
+            http_proxy.select_one("td:nth-of-type(2)").text,
+        )}
+    return proxy
