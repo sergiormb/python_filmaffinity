efb9615b78a4a86e904e55671d70f067d5d19f6f
Sergio Pino Márquez <sergiormb88@gmail.com>
2021-02-18 08:24:25 +0100

    New version

--- python_filmaffinity/__meta__.py
+++ python_filmaffinity/__meta__.py
@@ -5,7 +5,7 @@ __title__ = 'python_filmaffinity'
 __summary__ = 'Python wrapper for FilmAffinity'
 __url__ = 'https://github.com/sergiormb/python_filmaffinity'
 
-__version__ = '0.0.16'
+__version__ = '0.0.17'
 
 __install_requires__ = [
     'requests>=2.24.0',
